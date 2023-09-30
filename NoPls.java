57f5804463723c99086e47b36048a52d62c89891
hetsevalia <het.sun04@gmail.com>
2023-09-30 23:10:28 +0530

    Fixed bugs.

--- NoPls.java
+++ NoPls.java
@@ -1,6 +1,10 @@
 public class NoPls {
-    private static double main(Integer[] args) {
+    public static void main(String[] args) {
         System.out.println("I can never run :(");
         System.out.println("Or maybe.... I can? :O");
     }
 }
+/*
+ * 1. public static void main(String[] args) instead of private static double
+ * main(Integer[] args)
+ */
--- mistake.cpp
+++ mistake.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
-integer main() {
-    String correctMePls = 4;
+using namespace std;
+int main() {
+    char correctMePls = '4';
 
     return 45;
 }
+/* changes:
+line 3 - integer to int
+line 4 -String to char if we assign '4' we 4 as string, but when we assign 4 it is considered ASCII value.
+added 'using name std;', this code works without it as well. */
